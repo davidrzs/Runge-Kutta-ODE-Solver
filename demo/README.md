7f03a06abf7aeb834194f6c5a2719bfc74597ecc
David Zollikofer <david@zollikofer.co>
2020-04-08 10:01:59 +0200

    added lotka-volterra example

--- demo/README.md
+++ demo/README.md
@@ -1,1 +1,27 @@
-sudo apt-get install python-matplotlib python-numpy python2.7-dev
+# Demo: Solving and Plotting the Lotka-Volterra ODE
+
+## Short Explanation
+
+The [Lotka-Volterra ordinary differential equation (ODE)](https://en.wikipedia.org/wiki/Lotka%E2%80%93Volterra_equations) can be used to model the interaction between two species. 
+
+In this example we have defined a function `f` which takes a state `y0` and calculates the change in population according to the Lotka-Volterra Model. 
+
+Please note that I am not a biologist, hence the constants I picked in the equation might highly unrealistic.
+
+
+
+## How to Run the Example
+
+The project depends on [Eigen]() as well as [Matplotlib-Cpp](https://github.com/lava/matplotlib-cpp).
+
+Note that Matplotlib-Cpp uses Python 2, this isn't ideal but it is still a very good and lghtweight plotting library:
+
+### Fetch the Requires Packages
+
+To fetch the dependencies for Matplotlib-Cpp install the following packages (on debian systems for example) `sudo apt-get install python-matplotlib python-numpy python2.7-dev`.
+
+### Compile the File
+
+Having installed the prerequisites for Matplotlib-Cpp we can now compile our `demo.cpp` file using `g++ -I /usr/include/eigen3 -I /usr/include/python2.7  demo/demo.cpp -lpython2.7`. This of course assumes that you have an Eigen3 installation at `/usr/include/eigen3 `.
+
+
--- demo/demo.cpp
+++ demo/demo.cpp
@@ -12,6 +12,14 @@ namespace plt = matplotlibcpp;
 
 int main(void) {
 
+  lotkaVolterra();
+  lorenzAttractor();
+
+}
+
+
+void lotkaVolterra(){
+
   /**
    *  Setting global variables, the time we are integrating over
    *  and the number of integration steps we take in this time.
@@ -21,11 +29,12 @@ int main(void) {
    */
 
   unsigned int steps = 1000;
-  unsigned int time = 20;
+  unsigned int time = 13;
 
   auto f = [] (Eigen::VectorXd y) {
     Eigen::VectorXd df(2);
-    df << y(0)*(1-0.01*y(1)) , -y(1)*(1.5-0.2*y(0));
+    df << y(0)*(4-4.0/3*y(1)) , -y(1)*(0.8-0.4*y(0));
+
     return df;
   };
 
@@ -47,21 +56,72 @@ int main(void) {
         prey[i] = result[i](0);
         predator[i] = result[i](1);
     }
-    plt::title("Volterra-Lotka Integration Example");
+    plt::title("Lotka-Volterra Integration Example");
 
-    plt::named_plot("Prey",t, prey);
-    plt::named_plot("Predator",t,predator);
+    plt::named_plot("Prey Population",t, prey);
+    plt::named_plot("Predator Population",t,predator);
     
     plt::xlabel("Time");
     plt::ylabel("Population");
     plt::legend();
 
     plt::save("volterraLotkaSolved.png");
-
-
-
-  
   
 }
 
     
+
+
+
+void lorenzAttractor(){
+
+  /**
+   *  Setting global variables, the time we are integrating over
+   *  and the number of integration steps we take in this time.
+   * 
+   *  We also define the function f representing our ODE
+   *  and the initial conditiond y0.
+   */
+
+  unsigned int steps = 1000;
+  unsigned int time = 13;
+
+  auto f = [] (Eigen::VectorXd y) {
+    Eigen::VectorXd df(2);
+    df << y(0)*(4-4.0/3*y(1)) , -y(1)*(0.8-0.4*y(0));
+
+    return df;
+  };
+
+  Eigen::VectorXd y0(2);
+  y0 << 6,3;
+
+  /**
+   * Solve using built in solver:
+   */
+
+  std::vector<Eigen::VectorXd> result = ExplicitRKSolvers::classical4thOrderRuleIntegrator(f, time,y0, steps);
+
+  std::vector<double> t(steps);
+  std::vector<double> prey(steps);
+  std::vector<double> predator(steps);
+
+    for(size_t i = 0; i < t.size(); i++) {
+        t[i] = i*(time / (double) steps);
+        prey[i] = result[i](0);
+        predator[i] = result[i](1);
+    }
+    plt::title("Lotka-Volterra Integration Example");
+
+    plt::named_plot("Prey Population",t, prey);
+    plt::named_plot("Predator Population",t,predator);
+    
+    plt::xlabel("Time");
+    plt::ylabel("Population");
+    plt::legend();
+
+    plt::save("lorenzAttractorSolved.png");
+
+  
+  
+}
